9677000a54d84aae7085b2726338d1866569ec9b
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-07-30 22:11:27 -0300

    criação da tela home

---
+++ larahflix/src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png'
+
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Laraflix" />
+      </a>
+      <p>
+        Orgulhosamente criado com S2 e muito café | Eduardo Developer
+        {' '}
+        <a href="http://localhost:3000/">
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
