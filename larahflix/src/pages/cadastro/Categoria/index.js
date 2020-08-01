f4e4dc549fa6636954261e26cbe02a681faff529
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-08-01 18:00:09 -0300

    Formulários e componente 44:50

--- larahflix/src/pages/cadastro/Categoria/index.js
+++ larahflix/src/pages/cadastro/Categoria/index.js
@@ -1,13 +1,98 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(['Teste']);
+
+  const valueInitial = {
+    nome: 'Categoria inicial',
+    descricao: 'Descrição da Categoria',
+    cor: '#000',
+  }
+  const [values, setValues] = useState(valueInitial);
+
+  function setValue(key, value){
+    setValues({
+      ...values,
+      [key]: value,
+    })
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
+      <form onSubmit={
+        function handleSubmit(dataEvent){
+          dataEvent.preventDefault();
+          setCategorias([
+            ...categorias,
+            values
+          ]);
+
+        }
+      }>
+      {/* State */}
+        <div>
+            <label>
+              Nome da Categoria:
+            <input
+                   type="text"
+                   value={values.nome}
+                   onChange={
+                function functionHandler(dataEvent) {
+                  setValue('nome', dataEvent.target.value);
+                  // setNomeDaCategoria(dataEvent.target.value);
+                }} />
+            </label>
+
+        </div>
+
+          <div>
+            <label>
+              Descrição:
+            <textarea
+                type="text"
+              value={values.descricao}
+                onChange={
+                  function functionHandler(dataEvent) {
+                    // setNomeDaCategoria(dataEvent.target.value);
+                  }} />
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Cor:
+            <input
+                type="color"
+              value={values.cor}
+                onChange={
+                  function functionHandler(dataEvent) {
+                    // setNomeDaCategoria(dataEvent.target.value);
+                  }} />
+            </label>
+          </div>
+
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+      <ul>
+          {categorias.map((categoria, indice) => {
+            return(
+              <li key={`${categoria}${indice}`}>
+                {categoria}
+              </li>
+            )
+          })}
+      </ul>
+
+
       <Link to="/">
-        Ir para Home
+        Ir para home
       </Link>
     </PageDefault>
   )
